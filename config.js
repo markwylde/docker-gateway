ab9ea13134e161042523a7b34eb51b105dc8d20a
Mark Wylde <me@markwylde.com>
2021-08-14 16:53:50 +1000

    Use encoding for reading certs

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 module.exports = {
-  key: fs.readFileSync(process.env.SSL_KEY_PATH),
-  cert: fs.readFileSync(process.env.SSL_CERT_PATH),
-  ca: process.env.SSL_CHAIN_PATH && fs.readFileSync(process.env.SSL_CHAIN_PATH)
+  key: fs.readFileSync(process.env.SSL_KEY_PATH, 'utf8'),
+  cert: fs.readFileSync(process.env.SSL_CERT_PATH, 'utf8'),
+  ca: process.env.SSL_CHAIN_PATH && fs.readFileSync(process.env.SSL_CHAIN_PATH, 'utf8')
 };
