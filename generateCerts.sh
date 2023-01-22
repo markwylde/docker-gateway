70d0b0bde2935620271651035c9b19b92f331d71
Mark Wylde <me@markwylde.com>
2023-01-22 22:41:04 +0000

    Fix swarm

--- generateCerts.sh
+++ generateCerts.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 mkdir -p certs
-openssl genrsa -out certs/key.pem
-openssl req -newkey rsa:2048 -nodes -keyout certs/key.pem -x509 -days 3650 -out certs/cert.pem -subj "/C=GB/ST=London/L=London/O=Local Development/OU=IT Department/CN=localhost"
+openssl genrsa -out certs/one.test.key.pem
+openssl req -newkey rsa:2048 -nodes -keyout certs/one.test.key.pem -x509 -days 3650 -out certs/one.test.cert.pem -subj "/C=GB/ST=London/L=London/O=Local Development/OU=IT Department/CN=one.test"
+
+openssl genrsa -out certs/two.key.pem
+openssl req -newkey rsa:2048 -nodes -keyout certs/two.key.pem -x509 -days 3650 -out certs/two.cert.pem -subj "/C=GB/ST=London/L=London/O=Local Development/OU=IT Department/CN=two.test"
